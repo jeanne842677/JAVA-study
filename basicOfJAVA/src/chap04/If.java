a2673c34acbd536c784b76f8a9aab8de4324d798
jeanne8426 <jeanne842677@gmail.com>
2022-02-08 03:08:08 +0900

    helloworld

--- basicOfJAVA/src/chap04/If.java
+++ basicOfJAVA/src/chap04/If.java
@@ -1,5 +1,21 @@
 package chap04;
 
+import java.util.Scanner;
+
 public class If {
+	
+	public static void main(String[] args) {
+		
+		 Scanner scanner = new Scanner(System.in);
+		 int score = scanner.nextInt();
+		 
+		 
+		
+		if(score > 60) {
+			System.out.println("합격입니다.");
+		}
+		
+
+	}
 
 }
