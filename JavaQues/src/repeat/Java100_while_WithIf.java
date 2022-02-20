e25b29ad6b276590a1d5c342c1a919e7568a0bc5
jeanne8426 <jeanne842677@gmail.com>
2022-02-20 17:15:59 +0900

    commit

---
+++ JavaQues/src/repeat/Java100_while_WithIf.java
@@ -0,0 +1,13 @@
+package repeat;
+
+//홀수, 짝수 구하기 문제를 while 반복문을 사용하여 구현해보시오.
+//이 문제는 while 반복문과 그 안에서 조건문을 사용할 수 있는지를 묻는 문제이다.
+public class Java100_while_WithIf {
+	public static void main(String[] args) {
+		for(int i=2;i<30;i++) {
+				System.out.println(i);
+				i = i+1;
+		}
+	}
+
+}
